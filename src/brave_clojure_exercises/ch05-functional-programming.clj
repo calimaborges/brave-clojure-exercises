b91c7b46d29a490fd7e580836b263a50bd114d32
Carlos Augusto Borges <calimaborges@gmail.com>
2025-10-29 19:58:55 -0300

    feat: exercises 3 and 4 for chapter 05

--- src/brave_clojure_exercises/ch05-functional-programming.clj
+++ src/brave_clojure_exercises/ch05-functional-programming.clj
@@ -61,4 +61,32 @@
 
 (my-update-in character [:stats :attributes :dexterity] + 2)
 
+(def character
+  {:name "Smooches McCutes"
+   :stats {:attributes {:intelligence 10
+                        :strength 4
+                        :dexterity 5}}})
+
+(defn my-assoc-in
+  [m [k & ks] v]
+  (if (empty? ks)
+    (assoc m k v)
+    (assoc m k (my-assoc-in (k m) ks v))))
+
+(my-assoc-in character [:stats :attributes :vitality] 10)
+
+;;;;
+;; exercise 4
+
+(update-in character [:stats :attributes :dexterity] + 2)
+
+;;;;
+;; exercise 5
+
+(defn my-update-in
+  [m ks updater & args]
+  (assoc-in m ks (apply updater (get-in m ks) args)))
+
+(my-update-in character [:stats :attributes :dexterity] + 2)
+
 ;;;;
