8729848c51869a7cb8e910154aa7b5e01ee23e36
Carlos Augusto Borges <calimaborges@gmail.com>
2025-10-29 17:33:51 -0300

    feat: lein project skeleton

---
+++ test/brave_clojure_exercises/core_test.clj
@@ -0,0 +1,7 @@
+(ns brave-clojure-exercises.core-test
+  (:require [clojure.test :refer :all]
+            [brave-clojure-exercises.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
